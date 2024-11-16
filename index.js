765e3d834d25ee52ecd66911869ded7b71bd61b9
Maraah <maraahsang@gmail.com>
2024-11-16 08:51:54 +0000

    Setting up infrastructure to save the leads entered into the input field
    We create two variables, myLeads assigned to empty array and inputEl assigned to text input field using const.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
 function saveLead() {
     console.log("button clicked")
 }
 
 let inputBtn = document.getElementById("input-btn")
 inputBtn.addEventListener("click", function saveLead() {
-    console.log("Button clicked")
+    console.log("button clicked")
 })
