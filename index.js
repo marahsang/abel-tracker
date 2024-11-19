cc74f3f8b64ae0ba05b38a4533eeb080d23b1b6c
Maraah <maraahsang@gmail.com>
2024-11-19 09:29:03 +0000

    Refactor the code to render without hard-coding and clear input field
    We wrap the for-loop function in a renderLeads() function we create and call it inside the Event handler for the button clicks. That is inputBtn.addEventListener(click, function saveLead(){}). This way the items are pushed into the myLeads array and rendered out after the user enters the leads in the input field and presses the save input button. we also clear the input field after each entry.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 function saveLead() {
     console.log("button clicked")
@@ -9,15 +9,19 @@ const ulEl = document.getElementById("ul-el")
     inputBtn.addEventListener("click", function saveLead() {
         myLeads.push(inputEl.value)
         console.log(myLeads)
+        inputEl.value = ""
+        renderLeads()
     })
 
-    for (i = 0; i < myLeads.length; i++) {
-        console.log(myLeads)
-      //  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"        
-      //const li = document.createElement("li")
-      //li.textContent = myLeads[i]
-      //ulEl.append(li)
-      let listItems = ""
-      listItems += "<li>" + myLeads[i] + "</li>"
-      ulEl.innerHTML += listItems
+    function renderLeads() {
+        let listItems = ""
+        for (i = 0; i < myLeads.length; i++) {
+            console.log(myLeads)
+          //  ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"        
+          //const li = document.createElement("li")
+          //li.textContent = myLeads[i]
+          //ulEl.append(li)
+          listItems += "<li>" + myLeads[i] + "</li>"
     }
+    ulEl.innerHTML = listItems
+}
